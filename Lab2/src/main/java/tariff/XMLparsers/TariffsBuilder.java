bd7cd3820a9bacca96cd612ceb5edcaf2c41e787
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-12-08 01:10:47 +0200

    added sort

--- Lab2/src/main/java/tariff/XMLparsers/TariffsBuilder.java
+++ Lab2/src/main/java/tariff/XMLparsers/TariffsBuilder.java
@@ -3,6 +3,7 @@ package tariff.XMLparsers;
 import tariff.Tariff;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class TariffsBuilder {
@@ -13,6 +14,18 @@ public abstract class TariffsBuilder {
     }
 
     public List<Tariff> getTariffs() {
+        return tariffs;
+    }
+
+    public List<Tariff> getTariffsSorted() {
+        Comparator<Tariff> cmp = new Comparator<Tariff>() {
+            public int compare(Tariff lhs, Tariff rhs) {
+                return Integer.compare(lhs.getPayroll(), rhs.getPayroll());
+            }
+        };
+
+        tariffs.sort(cmp);
+
         return tariffs;
     }
 
