04ae485b3665336106b9146d298eba6eca3bbeba
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-12-08 01:43:38 +0200

    validator

---
+++ Lab2/src/main/java/tariff/XMLparsers/validator/TariffsXMLValidator.java
@@ -0,0 +1,26 @@
+package tariff.XMLparsers.validator;
+
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import javax.xml.transform.stream.StreamSource;
+
+public class TariffsXMLValidator {
+    public static boolean validate(String sourceFilename, String schemaFilename) {
+        try {
+            Schema schema = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema").newSchema(new File(schemaFilename));
+            schema.newValidator().validate(new StreamSource(new File(sourceFilename)));
+        } catch (SAXException e) {
+            System.out.println("SAX exception " + e);
+            return false;
+        } catch (IOException e) {
+            System.out.println("I/O exception " + e);
+            return false;
+        }
+        return true;
+    }
+}
