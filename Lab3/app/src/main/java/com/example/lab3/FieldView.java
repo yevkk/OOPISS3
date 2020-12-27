4f10f3d90163ba5444ee9c3bb37a592bd1bffe6a
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-12-28 01:54:22 +0200

    project initial commit

---
+++ Lab3/app/src/main/java/com/example/lab3/FieldView.java
@@ -0,0 +1,17 @@
+package com.example.lab3;
+
+import android.graphics.Canvas;
+import android.view.View;
+import android.content.Context;
+
+
+public class FieldView extends View {
+    public FieldView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
