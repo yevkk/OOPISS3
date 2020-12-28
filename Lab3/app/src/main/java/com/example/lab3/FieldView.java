0fe165e53f0e02ecb86894b8c3bbd10ec64f1c13
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-12-28 03:23:44 +0200

    FieldView onDraw override

--- Lab3/app/src/main/java/com/example/lab3/FieldView.java
+++ Lab3/app/src/main/java/com/example/lab3/FieldView.java
@@ -1,17 +1,55 @@
 package com.example.lab3;
 
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 import android.content.Context;
 
 
 public class FieldView extends View {
-    public FieldView(Context context) {
-        super(context);
+    private final Paint fieldPaint;
+    private float cellSize, cellDistance;
+    private float hPadding, vPadding;
+
+    public FieldView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        fieldPaint = new Paint();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+        hPadding = vPadding = 0;
+        float diff = this.getMeasuredHeight() - this.getMeasuredWidth();
+        if (diff < 0) {
+            hPadding = Math.abs(diff / 2f);
+        } else {
+            vPadding = Math.abs(diff / 2f);
+        }
+        cellSize = (this.getMeasuredWidth() - 2 * hPadding) / 13;
+        cellDistance = cellSize / 5;
+
+        fieldPaint.setColor(getResources().getColor(R.color.field_color, null));
+        fieldPaint.setTextAlign(Paint.Align.CENTER);
+        fieldPaint.setTextSize(cellSize * 0.6f);
+        fieldPaint.setStyle(Paint.Style.FILL);
+        fieldPaint.setAntiAlias(true);
+
+        float x = cellDistance + cellSize * 1.5f + hPadding;
+        float y = cellSize + vPadding;
+        for (int i = 1; i <= 10; i++, x += cellSize + cellDistance) {
+            canvas.drawText(String.valueOf(i), x, y, fieldPaint);
+        }
+
+        char symbol = 'A';
+        y = cellSize + cellDistance + vPadding;
+        for (int i = 1; i <= 10; i++, y += cellSize + cellDistance) {
+            x = cellSize * 0.5f + hPadding;
+            canvas.drawText(String.valueOf(symbol++), x, y + cellSize * 0.5f + cellDistance, fieldPaint);
+            x = cellSize + cellDistance + hPadding;
+            for (int j = 1; j <= 10; j++, x += cellSize + cellDistance) {
+                canvas.drawRect(x, y, x + cellSize, y + cellSize, fieldPaint);
+            }
+        }
     }
 }
