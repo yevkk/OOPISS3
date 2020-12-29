8c9f349aa32fd809a3c9df1d050b406d874b6653
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-12-29 04:09:03 +0200

    switched junit.jupiter to junit

--- Lab3/app/src/test/java/com/example/lab3/battleship/ShipTest.java
+++ Lab3/app/src/test/java/com/example/lab3/battleship/ShipTest.java
@@ -2,37 +2,37 @@ package com.example.lab3.battleship;
 
 import com.example.lab3.battleship.exceptions.ShipException;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ShipTest {
     @Test
     public void creatingWrongSize() {
-        Assertions.assertThrows(ShipException.class, () -> new Ship(-2, Ship.Orientation.VERTICAL, 0, 0));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(0, Ship.Orientation.VERTICAL, 0, 0));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(GameSetup.boardSize + 1, Ship.Orientation.VERTICAL, 0, 0));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(GameSetup.boardSize + 3, Ship.Orientation.VERTICAL, 0, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(-2, Ship.Orientation.VERTICAL, 0, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(0, Ship.Orientation.VERTICAL, 0, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(GameSetup.boardSize + 1, Ship.Orientation.VERTICAL, 0, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(GameSetup.boardSize + 3, Ship.Orientation.VERTICAL, 0, 0));
     }
 
     @Test
     public void creatingWrongCoordsSingle() {
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, -1, 0));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, GameSetup.boardSize, 0));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, GameSetup.boardSize + 3, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, -1, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, GameSetup.boardSize, 0));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, GameSetup.boardSize + 3, 0));
     }
 
     @Test
     public void creatingWrongCoordsRangeMin() {
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, -1));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize + 3));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, -1));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize + 3));
     }
 
     @Test
     public void creatingWrongCoordsRangeMax() {
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize - 1));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize - 1));
-        Assertions.assertThrows(ShipException.class, () -> new Ship(4, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize - 2));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize - 1));
+        Assert.assertThrows(ShipException.class, () -> new Ship(2, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize - 1));
+        Assert.assertThrows(ShipException.class, () -> new Ship(4, Ship.Orientation.VERTICAL, 0, GameSetup.boardSize - 2));
     }
 
     @Test
@@ -40,12 +40,12 @@ public class ShipTest {
         try {
             Ship ship = new Ship(1, Ship.Orientation.HORIZONTAL, 4, 3);
 
-            Assertions.assertEquals(1, ship.getSize());
-            Assertions.assertEquals(Ship.Orientation.HORIZONTAL, ship.getOrientation());
-            Assertions.assertEquals(4, ship.getCoordsSingle());
-            Assertions.assertEquals(3, ship.getCoordsRangeMin());
+            Assert.assertEquals(1, ship.getSize());
+            Assert.assertEquals(Ship.Orientation.HORIZONTAL, ship.getOrientation());
+            Assert.assertEquals(4, ship.getCoordsSingle());
+            Assert.assertEquals(3, ship.getCoordsRangeMin());
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -54,12 +54,12 @@ public class ShipTest {
         try {
             Ship ship = new Ship(2, Ship.Orientation.VERTICAL, 6, 5);
 
-            Assertions.assertEquals(2, ship.getSize());
-            Assertions.assertEquals(Ship.Orientation.VERTICAL, ship.getOrientation());
-            Assertions.assertEquals(6, ship.getCoordsSingle());
-            Assertions.assertEquals(5, ship.getCoordsRangeMin());
+            Assert.assertEquals(2, ship.getSize());
+            Assert.assertEquals(Ship.Orientation.VERTICAL, ship.getOrientation());
+            Assert.assertEquals(6, ship.getCoordsSingle());
+            Assert.assertEquals(5, ship.getCoordsRangeMin());
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -68,12 +68,12 @@ public class ShipTest {
         try {
             Ship ship = new Ship(3, Ship.Orientation.HORIZONTAL, 6, 6);
 
-            Assertions.assertEquals(3, ship.getSize());
-            Assertions.assertEquals(Ship.Orientation.HORIZONTAL, ship.getOrientation());
-            Assertions.assertEquals(6, ship.getCoordsSingle());
-            Assertions.assertEquals(6, ship.getCoordsRangeMin());
+            Assert.assertEquals(3, ship.getSize());
+            Assert.assertEquals(Ship.Orientation.HORIZONTAL, ship.getOrientation());
+            Assert.assertEquals(6, ship.getCoordsSingle());
+            Assert.assertEquals(6, ship.getCoordsRangeMin());
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -82,12 +82,12 @@ public class ShipTest {
         try {
             Ship ship = new Ship(4, Ship.Orientation.VERTICAL, 3, 0);
 
-            Assertions.assertEquals(4, ship.getSize());
-            Assertions.assertEquals(Ship.Orientation.VERTICAL, ship.getOrientation());
-            Assertions.assertEquals(3, ship.getCoordsSingle());
-            Assertions.assertEquals(0, ship.getCoordsRangeMin());
+            Assert.assertEquals(4, ship.getSize());
+            Assert.assertEquals(Ship.Orientation.VERTICAL, ship.getOrientation());
+            Assert.assertEquals(3, ship.getCoordsSingle());
+            Assert.assertEquals(0, ship.getCoordsRangeMin());
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -96,14 +96,14 @@ public class ShipTest {
         try {
             Ship ship = new Ship(2, Ship.Orientation.VERTICAL, 6, 5);
 
-            Assertions.assertTrue(ship.isPlacedOn(6, 5));
-            Assertions.assertTrue(ship.isPlacedOn(6, 6));
+            Assert.assertTrue(ship.isPlacedOn(6, 5));
+            Assert.assertTrue(ship.isPlacedOn(6, 6));
 
-            Assertions.assertFalse(ship.isPlacedOn(0, 0));
-            Assertions.assertFalse(ship.isPlacedOn(5, 7));
-            Assertions.assertFalse(ship.isPlacedOn(-4, 15));
+            Assert.assertFalse(ship.isPlacedOn(0, 0));
+            Assert.assertFalse(ship.isPlacedOn(5, 7));
+            Assert.assertFalse(ship.isPlacedOn(-4, 15));
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -112,16 +112,16 @@ public class ShipTest {
         try {
             Ship ship = new Ship(4, Ship.Orientation.HORIZONTAL, 3, 0);
 
-            Assertions.assertTrue(ship.isPlacedOn(0, 3));
-            Assertions.assertTrue(ship.isPlacedOn(1, 3));
-            Assertions.assertTrue(ship.isPlacedOn(2, 3));
-            Assertions.assertTrue(ship.isPlacedOn(3, 3));
+            Assert.assertTrue(ship.isPlacedOn(0, 3));
+            Assert.assertTrue(ship.isPlacedOn(1, 3));
+            Assert.assertTrue(ship.isPlacedOn(2, 3));
+            Assert.assertTrue(ship.isPlacedOn(3, 3));
 
-            Assertions.assertFalse(ship.isPlacedOn(0, 0));
-            Assertions.assertFalse(ship.isPlacedOn(3, 4));
-            Assertions.assertFalse(ship.isPlacedOn(-1, 3));
+            Assert.assertFalse(ship.isPlacedOn(0, 0));
+            Assert.assertFalse(ship.isPlacedOn(3, 4));
+            Assert.assertFalse(ship.isPlacedOn(-1, 3));
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -130,13 +130,13 @@ public class ShipTest {
         try {
             Ship ship = new Ship(2, Ship.Orientation.VERTICAL, 6, 5);
 
-            Assertions.assertTrue(ship.shoot(6, 5));
-            Assertions.assertTrue(ship.shoot(6, 6));
+            Assert.assertTrue(ship.shoot(6, 5));
+            Assert.assertTrue(ship.shoot(6, 6));
 
-            Assertions.assertFalse(ship.shoot(6, 5));
-            Assertions.assertFalse(ship.shoot(6, 6));
+            Assert.assertFalse(ship.shoot(6, 5));
+            Assert.assertFalse(ship.shoot(6, 6));
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -145,17 +145,17 @@ public class ShipTest {
         try {
             Ship ship = new Ship(4, Ship.Orientation.HORIZONTAL, 3, 0);
 
-            Assertions.assertTrue(ship.shoot(0, 3));
-            Assertions.assertTrue(ship.shoot(1, 3));
-            Assertions.assertTrue(ship.shoot(2, 3));
-            Assertions.assertTrue(ship.shoot(3, 3));
+            Assert.assertTrue(ship.shoot(0, 3));
+            Assert.assertTrue(ship.shoot(1, 3));
+            Assert.assertTrue(ship.shoot(2, 3));
+            Assert.assertTrue(ship.shoot(3, 3));
 
-            Assertions.assertFalse(ship.shoot(0, 3));
-            Assertions.assertFalse(ship.shoot(1, 3));
-            Assertions.assertFalse(ship.shoot(2, 3));
-            Assertions.assertFalse(ship.shoot(3, 3));
+            Assert.assertFalse(ship.shoot(0, 3));
+            Assert.assertFalse(ship.shoot(1, 3));
+            Assert.assertFalse(ship.shoot(2, 3));
+            Assert.assertFalse(ship.shoot(3, 3));
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -164,11 +164,11 @@ public class ShipTest {
         try {
             Ship ship = new Ship(2, Ship.Orientation.VERTICAL, 6, 5);
 
-            Assertions.assertFalse(ship.shoot(0, 0));
-            Assertions.assertFalse(ship.shoot(5, 7));
-            Assertions.assertFalse(ship.shoot(-4, 15));
+            Assert.assertFalse(ship.shoot(0, 0));
+            Assert.assertFalse(ship.shoot(5, 7));
+            Assert.assertFalse(ship.shoot(-4, 15));
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -177,11 +177,11 @@ public class ShipTest {
         try {
             Ship ship = new Ship(4, Ship.Orientation.HORIZONTAL, 3, 0);
 
-            Assertions.assertFalse(ship.shoot(0, 0));
-            Assertions.assertFalse(ship.shoot(3, 4));
-            Assertions.assertFalse(ship.shoot(-1, 3));
+            Assert.assertFalse(ship.shoot(0, 0));
+            Assert.assertFalse(ship.shoot(3, 4));
+            Assert.assertFalse(ship.shoot(-1, 3));
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -191,11 +191,11 @@ public class ShipTest {
             Ship ship = new Ship(2, Ship.Orientation.HORIZONTAL, 5, 6);
 
             ship.shoot(6, 5);
-            Assertions.assertFalse(ship.isDead());
+            Assert.assertFalse(ship.isDead());
             ship.shoot(7, 5);
-            Assertions.assertTrue(ship.isDead());
+            Assert.assertTrue(ship.isDead());
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 
@@ -205,15 +205,15 @@ public class ShipTest {
             Ship ship = new Ship(4, Ship.Orientation.VERTICAL, 0, 3);
 
             ship.shoot(0, 3);
-            Assertions.assertFalse(ship.isDead());
+            Assert.assertFalse(ship.isDead());
             ship.shoot(0, 4);
-            Assertions.assertFalse(ship.isDead());
+            Assert.assertFalse(ship.isDead());
             ship.shoot(0, 5);
-            Assertions.assertFalse(ship.isDead());
+            Assert.assertFalse(ship.isDead());
             ship.shoot(0, 6);
-            Assertions.assertTrue(ship.isDead());
+            Assert.assertTrue(ship.isDead());
         } catch (ShipException e) {
-            Assertions.fail("unexpected ShipException");
+            Assert.fail("unexpected ShipException");
         }
     }
 }
